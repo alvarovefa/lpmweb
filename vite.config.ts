467f1132aeecc31753ec511cb42b742e3f18e75d
alvarovefa <alvaro.vergara.f@gmail.com>
2026-06-01 21:02:44 -0400

    rutas

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     base: command === 'build' ? '/lpmweb/' : '/',
     server: {
       host: '0.0.0.0',
