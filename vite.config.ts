e5a8311dbec087f7ebd1123f72c72bfb846aaa58
alvarovefa <alvaro.vergara.f@gmail.com>
2026-06-03 20:27:37 -0400

    rutas estáticas

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(({ command }) => {
   return {
     plugins: [tailwindcss()],
-    base: command === 'build' ? '/lpmweb/' : '/',
+    base: command === 'build' ? './' : '/',
     server: {
       host: '0.0.0.0',
       port: 3000,
